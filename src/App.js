506caae5171fbc3f546d6c03d7520352b94de3f1
Tabrez Khan <khan.t.29@gmail.com>
2019-12-08 22:25:59 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Testing continuos deployment for Heroku with Github Actions!
+      Testing continuous deployment for Heroku with Github Actions! A change
     </div>
   );
 }
